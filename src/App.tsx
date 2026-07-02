dc6653dfb8eb2b388e48b60f04b2501c4d605f65
eissaalbothigi-cell <eissaalbothigi@gmail.com>
2026-07-02 11:42:44 +0300

    Add App component with routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,28 @@
+import { Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import PromptGenerator from './pages/PromptGenerator'
+import PromptLibrary from './pages/PromptLibrary'
+import Login from './pages/Login'
+import './App.css'
 
+function App() {
+  return (
+    <AuthProvider>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/generator" element={<PromptGenerator />} />
+          <Route path="/library" element={<PromptLibrary />} />
+        </Routes>
+      </div>
+    </AuthProvider>
+  )
+}
+
+export default App
